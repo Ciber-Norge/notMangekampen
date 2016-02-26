49c4e959efe70d0c7950648eca9d4ac848bc0144
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2016-02-26 15:23:27 +0100

    Working on adding a database

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
 require 'roda'
 
 unless KEEN_PROJECT_ID = ENV['KEEN_PROJECT_ID']
-  raise "You must specify the KEEN_PROJECT_ID env variable"
+#  raise "You must specify the KEEN_PROJECT_ID env variable"
 end
 
 unless KEEN_WRITE_KEY = ENV['KEEN_WRITE_KEY']
-  raise "You must specify the KEEN_WRITE_KEY env variable"
+#  raise "You must specify the KEEN_WRITE_KEY env variable"
 end
 
 unless KEEN_API_URL = ENV['KEEN_API_URL']
-  raise "You must specify the KEEN_API_URL env variable"
+#  raise "You must specify the KEEN_API_URL env variable"
 end
 
 unless DATABASE_URL = ENV['DATABASE_URL']
-  raise "You must specify the DATABASE_URL env variable"
+#  raise "You must specify the DATABASE_URL env variable"
 end
 
 
@@ -26,6 +26,26 @@ class NotMangekampen < Roda
   route do |r|
     r.root do
       render('index')
+    end
+
+    r.on 'challenge' do
+      r.get ':id' do | id |
+        challenge = Challenge.find('uuid = ?', id)
+        # logging
+        challenge.html
+      end
+
+      r.post ':id' do | id |
+        challenge = Challenge.find('uuid = ?', id)
+        # logging
+        if challenge.answer == r.params[:answer]
+          # logg
+          redirect("/challenge/#{challenge.next}")
+        else
+          # logg
+          redirect("/challenge/#{id}")
+        end
+      end
     end
   end
 end
